96581175bc9b215c078a8628fe481c6aa72f29fe
Amandine M <amandine.moigno@gmail.com>
2026-07-23 17:14:24 +0200

    feat: add react-router-dom

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,6 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import "./Layout.css";
 
 export default function Layout() {
   return (
